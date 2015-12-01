06b6155788dacf16443113d42912253753ab6542
Pavel Pavlenko <region23@gmail.com>
2015-12-02 01:11:23 +0300

    fix import path

--- server.go
+++ server.go
@@ -9,8 +9,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/pavlik/mimozaflowers/Godeps/_workspace/src/github.com/gin-gonic/gin"
-	"github.com/pavlik/mimozaflowers/Godeps/_workspace/src/github.com/spf13/viper"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 type config struct {
